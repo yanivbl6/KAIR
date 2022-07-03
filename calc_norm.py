76baf2a1092f1f00bc9e2f64bf2c1d91c251f964
Yaniv Blumenfeld <yanivblm6@gmail.com>
2022-07-03 15:49:32 +0300

    added calc norm code and updated the new_norm config

---
+++ calc_norm.py
@@ -0,0 +1,154 @@
+import os.path
+import math
+import argparse
+import time
+import random
+import numpy as np
+from collections import OrderedDict
+import logging
+import torch
+from torch.utils.data import DataLoader
+
+
+from utils import utils_logger
+from utils import utils_image as util
+from utils import utils_option as option
+
+from data.select_dataset import define_Dataset
+from models.select_model import define_Model
+
+
+
+
+def norm_change(img, new_norm):
+    frac = new_norm / (torch.norm(img))
+    return img*frac
+
+
+'''
+# --------------------------------------------
+# training code for DnCNN
+# --------------------------------------------
+# Kai Zhang (cskaizhang@gmail.com)
+# github: https://github.com/cszn/KAIR
+#         https://github.com/cszn/DnCNN
+#
+# Reference:
+@article{zhang2017beyond,
+  title={Beyond a gaussian denoiser: Residual learning of deep cnn for image denoising},
+  author={Zhang, Kai and Zuo, Wangmeng and Chen, Yunjin and Meng, Deyu and Zhang, Lei},
+  journal={IEEE Transactions on Image Processing},
+  volume={26},
+  number={7},
+  pages={3142--3155},
+  year={2017},
+  publisher={IEEE}
+}
+# --------------------------------------------
+# https://github.com/xinntao/BasicSR
+# --------------------------------------------
+'''
+
+def main(json_path='options/train_dncnn.json'):
+
+
+    dataset = 'bsd'
+    gray_scale = False
+    new_norm = 45.167343  # mean of the norm of bsd images with patches of 80*80
+    input_channels = 3
+    lambd = lambda x: norm_change(x, new_norm)
+
+    '''
+    # ----------------------------------------
+    # Step--1 (prepare opt)
+    # ----------------------------------------
+    '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-opt', type=str, default=json_path, help='Path to option JSON file.')
+
+    opt = option.parse(parser.parse_args().opt, is_train=True)
+    util.mkdirs((path for key, path in opt['path'].items() if 'pretrained' not in key))
+
+    # ----------------------------------------
+    # update opt
+    # ----------------------------------------
+    # -->-->-->-->-->-->-->-->-->-->-->-->-->-
+    init_iter, init_path_G = option.find_last_checkpoint(opt['path']['models'], net_type='G')
+    opt['path']['pretrained_netG'] = init_path_G
+    current_step = init_iter
+
+    border = 0
+    # --<--<--<--<--<--<--<--<--<--<--<--<--<-
+
+    # ----------------------------------------
+    # save opt to  a '../option.json' file
+    # ----------------------------------------
+    option.save(opt)
+
+    # ----------------------------------------
+    # return None for missing key
+    # ----------------------------------------
+    opt = option.dict_to_nonedict(opt)
+
+    # ----------------------------------------
+    # configure logger
+    # ----------------------------------------
+    logger_name = 'train'
+    utils_logger.logger_info(logger_name, os.path.join(opt['path']['log'], logger_name+'.log'))
+    logger = logging.getLogger(logger_name)
+    logger.info(option.dict2str(opt))
+
+    # ----------------------------------------
+    # seed
+    # ----------------------------------------
+    seed = opt['train']['manual_seed']
+    if seed is None:
+        seed = random.randint(1, 10000)
+    logger.info('Random seed: {}'.format(seed))
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    '''
+    # ----------------------------------------
+    # Step--2 (creat dataloader)
+    # ----------------------------------------
+    '''
+
+    # ----------------------------------------
+    # 1) create_dataset
+    # 2) creat_dataloader for train and test
+    # ----------------------------------------
+    dataset_type = opt['datasets']['train']['dataset_type']
+    for phase, dataset_opt in opt['datasets'].items():
+        if phase == 'train':
+            train_set = define_Dataset(dataset_opt)
+            train_size = int(math.ceil(len(train_set) / dataset_opt['dataloader_batch_size']))
+            logger.info('Number of train images: {:,d}, iters: {:,d}'.format(len(train_set), train_size))
+            train_loader = DataLoader(train_set,
+                                      batch_size=1,
+                                      shuffle=dataset_opt['dataloader_shuffle'],
+                                      num_workers=dataset_opt['dataloader_num_workers'],
+                                      drop_last=True,
+                                      pin_memory=True)
+        elif phase == 'test':
+            test_set = define_Dataset(dataset_opt)
+            test_loader = DataLoader(test_set, batch_size=1,
+                                     shuffle=False, num_workers=1,
+                                     drop_last=False, pin_memory=True)
+        else:
+            raise NotImplementedError("Phase [%s] is not recognized." % phase)
+
+
+
+    norms = np.zeros([len(train_loader)])
+    for i, train_data in enumerate(train_loader):
+        norms[i] = np.linalg.norm(train_data['H'].squeeze())
+
+    print(np.mean(norms))
+
+
+if __name__ == '__main__':
+    main()
